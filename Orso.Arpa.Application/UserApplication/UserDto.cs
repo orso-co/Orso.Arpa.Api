2ced67a2930e278f0895c3718209f773e6f74365
VILLAN3LL3 <development@gutfrau.de>
2021-02-13 11:51:18 +0100

    ARPA-106 Added createdAt property for user

--- Orso.Arpa.Application/UserApplication/UserDto.cs
+++ Orso.Arpa.Application/UserApplication/UserDto.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using AutoMapper;
+using Orso.Arpa.Application.Extensions;
 using Orso.Arpa.Domain.Entities;
 
 namespace Orso.Arpa.Application.UserApplication
@@ -11,6 +12,7 @@ namespace Orso.Arpa.Application.UserApplication
         public string DisplayName { get; set; }
         public string Email { get; set; }
         public bool EmailConfirmed { get; set; }
+        public string CreatedAt { get; set; }
     }
 
     public class UserDtoMappingProfile : Profile
@@ -18,6 +20,7 @@ namespace Orso.Arpa.Application.UserApplication
         public UserDtoMappingProfile()
         {
             CreateMap<User, UserDto>()
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt.ToIsoString()))
                 .ForMember(dest => dest.RoleNames, opt => opt.Ignore());
         }
     }
--- Tests/Orso.Arpa.Application.Tests/MappingProfileTests/UserDtoMappingProfileTests.cs
+++ Tests/Orso.Arpa.Application.Tests/MappingProfileTests/UserDtoMappingProfileTests.cs
@@ -33,7 +33,10 @@ namespace Orso.Arpa.Application.Tests.MappingProfileTests
 
             // Assert
             dto.Should().BeEquivalentTo(expectedDto, opt => opt
-                .Excluding(dest => dest.RoleNames));
+
+                .Excluding(dest => dest.RoleNames))
+                .Excluding(dest => dest.CreatedAt));
+            dto.CreatedAt.Should().NotBeNullOrEmpty();
         }
     }
 }
