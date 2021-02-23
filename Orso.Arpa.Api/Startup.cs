22edebb5b23226657c940467ddbe147cb53f2241
code-puzzler <56153657+code-puzzler@users.noreply.github.com>
2021-02-23 20:49:11 +0100

    Update Startup.cs

--- Orso.Arpa.Api/Startup.cs
+++ Orso.Arpa.Api/Startup.cs
@@ -358,7 +358,6 @@ namespace Orso.Arpa.Api
             EnsureDatabaseMigrations(app);
         }
 
-        
         private static void AddSwagger(IApplicationBuilder app)
         {
             app.UseSwagger();
