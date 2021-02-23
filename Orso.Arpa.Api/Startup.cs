7140888a83a94eb6519e5671d2b81aaf240fc741
code-puzzler <56153657+code-puzzler@users.noreply.github.com>
2021-02-23 20:49:11 +0100

    Update Startup.cs

--- Orso.Arpa.Api/Startup.cs
+++ Orso.Arpa.Api/Startup.cs
@@ -393,7 +393,6 @@ namespace Orso.Arpa.Api
             app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
 
-        
         private static void AddSwagger(IApplicationBuilder app)
         {
             app.UseScopedSwagger(); // Workaround https://github.com/micro-elements/MicroElements.Swashbuckle.FluentValidation#error-systeminvalidoperationexception-cannot-resolve-ivalidatort-from-root-provider-because-it-requires-scoped-service-tdependency
