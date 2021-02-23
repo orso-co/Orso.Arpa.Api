3c296bd72693f6827b43fd10e54b2d82a6e3d8a6
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2021-02-23 19:54:39 +0100

    ARPA-146 Changed user deletion to hard deletion (#93)

---
+++ Orso.Arpa.Persistence/Migrations/20210222175117_RemoveDeletedFlagFromUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    public partial class RemoveDeletedFlagFromUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
