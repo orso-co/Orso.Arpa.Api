b592338678cee405c6055f9eff70ad814605bd99
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2021-02-28 14:41:32 +0100

    ARPA-149 Send email after password change (#102)

---
+++ Tests/Orso.Arpa.Domain.Tests/AuthTests/ValidatorTests/SendPasswordChangedInfoCommandValidatorTests.cs
@@ -0,0 +1,41 @@
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+using Orso.Arpa.Domain.Identity;
+using Orso.Arpa.Domain.Logic.Auth;
+using Orso.Arpa.Tests.Shared.Identity;
+using Orso.Arpa.Tests.Shared.TestSeedData;
+
+namespace Orso.Arpa.Domain.Tests.AuthTests.ValidatorTests
+{
+    [TestFixture]
+    public class SendPasswordChangedInfoCommandValidatorTests
+    {
+        private SendPasswordChangedInfo.Validator _validator;
+        private ArpaUserManager _userManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userManager = new FakeUserManager();
+            _validator = new SendPasswordChangedInfo.Validator(_userManager);
+        }
+
+        [Test]
+        public void Should_Not_Have_Validation_Error_If_Username_Does_Exist()
+        {
+            _validator.ShouldNotHaveValidationErrorFor(command => command.UsernameOrEmail, UserTestSeedData.Performer.UserName);
+        }
+
+        [Test]
+        public void Should_Have_Validation_Error_If_Username_Does_Not_Exist()
+        {
+            _validator.ShouldHaveValidationErrorFor(c => c.UsernameOrEmail, "DoesNotExist");
+        }
+
+        [Test]
+        public void Should_Not_Have_Validation_Error_If_Username_Is_Empty([Values(null, "")]string username)
+        {
+            _validator.ShouldNotHaveValidationErrorFor(command => command.UsernameOrEmail, username);
+        }
+    }
+}
