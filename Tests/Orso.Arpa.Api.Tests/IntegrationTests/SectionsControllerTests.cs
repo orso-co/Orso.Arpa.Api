a069f69e6ff946144db608c3166e725dc9f64df0
Wolfgang Roese <mail@wolfgangroese.de>
2021-03-19 19:30:42 +0100

    SectionSeedData: more Sections created:  (#115)
    * SectionSeedData: more Sections created: Conductor, Brass, Woodwinds, Strings, Others, Voices, and many more
    * tests and test data fixed
    * ensured that already used GUIDs are still there. In case of a removal of a section, the GUID was reused for a different section (this is crutial not to leave dead GUIDs in any test database on somebodys machine)
    
    * Documentation extended: how to downgrade the SQL database to a former migration & how to rollback an already applied migration from the code base
    
    Co-authored-by: VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
    Co-authored-by: Erich <79328248+euphi967@users.noreply.github.com>

--- Tests/Orso.Arpa.Api.Tests/IntegrationTests/SectionsControllerTests.cs
+++ Tests/Orso.Arpa.Api.Tests/IntegrationTests/SectionsControllerTests.cs
@@ -27,6 +27,8 @@ namespace Orso.Arpa.Api.Tests.IntegrationTests
             // Assert
             responseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
             IEnumerable<SectionDto> result = await DeserializeResponseMessageAsync<IEnumerable<SectionDto>>(responseMessage);
+            // TODO: result contains the full Section tree of currently 118 sections,
+            //       the static SectionDtoData contains a different, smaller tree
             result.Should().BeEquivalentTo(SectionDtoData.Sections);
         }
 
@@ -42,6 +44,8 @@ namespace Orso.Arpa.Api.Tests.IntegrationTests
             // Assert
             responseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
             SectionTreeDto result = await DeserializeResponseMessageAsync<SectionTreeDto>(responseMessage);
+            // TODO: result contains the full Section tree of currently 118 sections,
+            //       whereas the SectionTreeDtoData.Level2SectionTreeDto only a test tree with some elements
             result.Should().BeEquivalentTo(SectionTreeDtoData.Level2SectionTreeDto);
         }
 
