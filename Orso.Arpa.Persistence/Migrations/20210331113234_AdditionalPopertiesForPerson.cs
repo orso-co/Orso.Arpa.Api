b3d2adb51a8bd40a042247aa2ef380864ca16772
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2021-03-31 18:25:51 +0200

    ARPA-234 Added fields to Person Table. Changed Endpoint Authorization according to Wolfgang's preferences (#134)

---
+++ Orso.Arpa.Persistence/Migrations/20210331113234_AdditionalPopertiesForPerson.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    public partial class AdditionalPopertiesForPerson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Urls_Projects_ProjectId",
+                table: "Urls");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProjectId",
+                table: "Urls",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AboutMe",
+                table: "Persons",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Favorization",
+                table: "Persons",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Reliability",
+                table: "Persons",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Urls_Projects_ProjectId",
+                table: "Urls",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Urls_Projects_ProjectId",
+                table: "Urls");
+
+            migrationBuilder.DropColumn(
+                name: "AboutMe",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Favorization",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Reliability",
+                table: "Persons");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProjectId",
+                table: "Urls",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Urls_Projects_ProjectId",
+                table: "Urls",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
