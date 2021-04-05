bbd5c80602054a78d90f25ac193ebac590dbc460
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2021-04-05 14:05:49 +0200

    Feature/arpa 272 (#138)
    * ARPA-272 Added several properties and navigations to MusicianProfile
    
    Co-authored-by: Erich <79328248+euphi967@users.noreply.github.com>

---
+++ Orso.Arpa.Persistence/Migrations/20210405093716_DeleteBehaviorForRefreshToken.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    public partial class DeleteBehaviorForRefreshToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_UserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_UserId",
+                table: "RefreshTokens",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
