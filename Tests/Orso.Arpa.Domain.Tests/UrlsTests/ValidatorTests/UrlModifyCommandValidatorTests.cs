8aaf40c3810035b3323c53c5885d777d5aac843a
Erich <79328248+euphi967@users.noreply.github.com>
2021-04-10 10:11:09 +0200

    useage of mocking with _arpaContext.EntityExistsAsync introduced in unit test

--- Tests/Orso.Arpa.Domain.Tests/UrlsTests/ValidatorTests/UrlModifyCommandValidatorTests.cs
+++ Tests/Orso.Arpa.Domain.Tests/UrlsTests/ValidatorTests/UrlModifyCommandValidatorTests.cs
@@ -1,11 +1,10 @@
 using System;
+using System.Threading;
 using FluentValidation.TestHelper;
-using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using NUnit.Framework;
 using Orso.Arpa.Domain.Entities;
 using Orso.Arpa.Domain.Interfaces;
-using Orso.Arpa.Tests.Shared.FakeData;
 using Orso.Arpa.Tests.Shared.TestSeedData;
 using static Orso.Arpa.Domain.Logic.Urls.Modify;
 
@@ -16,15 +15,11 @@ namespace Orso.Arpa.Domain.Tests.UrlTests.ValidatorTests
     {
         private IArpaContext _arpaContext;
         private Validator _validator;
-        private DbSet<Url> _mockUrlDbSet;
 
         [SetUp]
         public void SetUp()
         {
             _arpaContext = Substitute.For<IArpaContext>();
-            _mockUrlDbSet = MockDbSets.Urls;
-
-            _arpaContext.Set<Url>().Returns(_mockUrlDbSet);
 
             _validator = new Validator(_arpaContext);
         }
@@ -32,12 +27,14 @@ namespace Orso.Arpa.Domain.Tests.UrlTests.ValidatorTests
         [Test]
         public void Should_Not_Have_Validation_Error_If_Valid_Id_Is_Supplied()
         {
+            _arpaContext.EntityExistsAsync<Url>(Arg.Any<Guid>(), Arg.Any<CancellationToken>()).Returns(true);
             _validator.ShouldNotHaveValidationErrorFor(c => c.Id, UrlSeedData.ArpaWebsite.Id);
         }
 
         [Test]
         public void Should_Have_Validation_Error_If_Id_Does_Not_Exist()
         {
+            _arpaContext.EntityExistsAsync<Url>(Arg.Any<Guid>(), Arg.Any<CancellationToken>()).Returns(false);
             _validator.ShouldHaveValidationErrorFor(c => c.Id, Guid.NewGuid());
         }
     }
