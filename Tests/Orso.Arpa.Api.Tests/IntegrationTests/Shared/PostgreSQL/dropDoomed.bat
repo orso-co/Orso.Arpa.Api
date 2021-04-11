afb4b4ad83de1423592274914104e90bcac21de6
Erich <79328248+euphi967@users.noreply.github.com>
2021-04-12 00:04:35 +0200

    provided a README.MD for the api tests talking about server settings for the PostgreSQL server to optimize performance for integration tests. provided a batch file to remove orphaned test databases (doomed_database*).

---
+++ Tests/Orso.Arpa.Api.Tests/IntegrationTests/Shared/PostgreSQL/dropDoomed.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem ------------------------------------------------------------------
+rem created by: Erich Hermann, 2021-04-11
+rem 
+rem drops all *doomed_database* databases from the PostgreSQL server
+rem ------------------------------------------------------------------
+
+setlocal
+pushd %ProgramFiles%\PostgreSQL\13\bin
+
+rem TODO: set your secret PostgreSQL password for the user 'postgres' here
+set PGPASSWORD=Pa$$w0rd
+
+rem lets get rid of all doomed databases
+for /f "tokens=1*" %%a in ('psql -c "\l" -U postgres ^| grep doomed_database*') do dropdb -f -U postgres %%a
