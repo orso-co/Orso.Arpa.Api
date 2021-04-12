bb645baadc3796288a3490934a4d41162225e91b
Erich <79328248+euphi967@users.noreply.github.com>
2021-04-12 16:20:12 +0200

    updated password to commonly in dev team used one

--- Tests/Orso.Arpa.Api.Tests/IntegrationTests/Shared/PostgreSQL/dropDoomed.bat
+++ Tests/Orso.Arpa.Api.Tests/IntegrationTests/Shared/PostgreSQL/dropDoomed.bat
@@ -10,7 +10,7 @@ setlocal
 pushd %ProgramFiles%\PostgreSQL\13\bin
 
 rem TODO: set your secret PostgreSQL password for the user 'postgres' here
-set PGPASSWORD=Pa$$w0rd
+set PGPASSWORD=postgres
 
 rem lets get rid of all doomed databases
 for /f "tokens=1*" %%a in ('psql -c "\l" -U postgres ^| grep doomed_database*') do dropdb -f -U postgres %%a
