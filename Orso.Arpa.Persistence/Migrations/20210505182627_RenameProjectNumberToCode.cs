c7bd7aed6c6c5470175a30fa0f4e7c728fbec254
Erich <79328248+euphi967@users.noreply.github.com>
2021-05-23 22:18:01 +0200

    ARPA-332-Renamed Project.Number to Project.Code (#174)

---
+++ Orso.Arpa.Persistence/Migrations/20210505182627_RenameProjectNumberToCode.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    public partial class RenameProjectNumberToCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "number",
+                table: "projects",
+                newName: "code");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_projects_number",
+                table: "projects",
+                newName: "ix_projects_code");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "code",
+                table: "projects",
+                newName: "number");
+
+            migrationBuilder.RenameIndex(
+                name: "ix_projects_code",
+                table: "projects",
+                newName: "ix_projects_number");
+        }
+    }
+}
