d7e5ce47db6bfad65413aef9b92477c9f3623285
Sascha Seewald <sascha.seewald@me.com>
2021-08-26 15:19:43 +0200

    Add case insensitive filtering

---
+++ Orso.Arpa.Api/GraphQL/CustomFilteringConvention.cs
@@ -0,0 +1,19 @@
+using HotChocolate.Data;
+using HotChocolate.Data.Filters;
+using HotChocolate.Data.Filters.Expressions;
+
+namespace Orso.Arpa.Api.GraphQL
+{
+    public class CustomFilteringConvention : FilterConvention
+    {
+        protected override void Configure(IFilterConventionDescriptor descriptor)
+        {
+            descriptor.AddDefaults();
+            descriptor.Operation(DefaultFilterOperations.Equals).Name("equals");
+            descriptor.AddProviderExtension(
+                new QueryableFilterProviderExtension(x => {
+                x.AddFieldHandler<QueryableStringInvariantEqualsHandler>();
+            }));
+        }
+    }
+}
