14bce0f93f652be9dcb513319cf576bcc3ceb248
VILLAN3LL3 <development@gutfrau.de>
2021-12-16 20:24:56 +0100

    Re-added localization tests

--- Tests/Orso.Arpa.Api.Tests/IntegrationTests/TranslationResultFilterTests.cs
+++ Tests/Orso.Arpa.Api.Tests/IntegrationTests/TranslationResultFilterTests.cs
@@ -1,5 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 using Orso.Arpa.Api.Tests.IntegrationTests.Shared;
+using Orso.Arpa.Application.RoleApplication;
+using Orso.Arpa.Application.SectionApplication;
+using Orso.Arpa.Tests.Shared.DtoTestData;
 
 namespace Orso.Arpa.Api.Tests.IntegrationTests
 {
@@ -12,49 +20,53 @@ namespace Orso.Arpa.Api.Tests.IntegrationTests
 
         }
 
-        // ToDo https://orso.atlassian.net/browse/ARPA-463?atlOrigin=eyJpIjoiOWY5Zjc2ZjMyZWFhNGNkNDgwZTI5M2FkZTc5ODEwYTciLCJwIjoiamlyYS1zbGFjay1pbnQifQ
+        [Test, Order(1)]
+        [TestCase("de-DE", "Mitwirkende", "Orchester")]
+        [TestCase("de", "Mitwirkende", "Orchester")]
+        [TestCase("en", "Performers", "Orchestra")]
+        [TestCase("en,de-DE;q=0.7,de;q=0.3", "Performers", "Orchestra")]
+        public async Task Should_Translate_Single_Section(string header, string firstCheck, string secondCheck)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, ApiEndpoints.SectionsController.Get());
+            requestMessage.Headers.Add("Accept-Language", header);
 
-        //[Test, Order(1)]
-        //public async Task Should_Translate_Single_Section()
-        //{
-        //    var requestMessage = new HttpRequestMessage(HttpMethod.Get,ApiEndpoints.SectionsController.Get());
-        //    requestMessage.Headers.Add("Accept-Language", "de-DE");
+            HttpResponseMessage responseMessage = await _authenticatedServer
+                .CreateClient()
+                .AuthenticateWith(_performer)
+                .SendAsync(requestMessage);
 
-        //    HttpResponseMessage responseMessage = await _authenticatedServer
-        //        .CreateClient()
-        //        .AuthenticateWith(_performer)
-        //        .SendAsync(requestMessage);
+            IList<SectionDto> result = await DeserializeResponseMessageAsync<IList<SectionDto>>(responseMessage);
 
-        //    IList<SectionDto> result = await DeserializeResponseMessageAsync<IList<SectionDto>>(responseMessage);
+            result
+                .First(s => s.Id == SectionDtoData.Performers.Id)
+                .Name.Should().Be(firstCheck);
 
-        //    result.AsQueryable().Where(s => s.Id == SectionDtoData.Performers.Id).Select(s => s.Name)
-        //        .All(s => s.Equals("Künstler")).Should().BeTrue();
+            result
+                .First(s => s.Id == SectionDtoData.Orchestra.Id)
+                .Name.Should().Be(secondCheck);
+        }
 
-        //    result.AsQueryable().Where(s => s.Id == SectionDtoData.Orchestra.Id).Select(s => s.Name)
-        //        .All(s => s.Equals("Orchester")).Should().BeTrue();
-        //}
+        [Test, Order(2)]
+        public async Task Should_Translate_Roles()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, ApiEndpoints.RolesController.Get());
+            requestMessage.Headers.Add("Accept-Language", "de-DE");
 
-        //[Test, Order(2)]
-        //public async Task Should_Translate_Roles()
-        //{
-        //    var requestMessage = new HttpRequestMessage(HttpMethod.Get,ApiEndpoints.RolesController.Get());
-        //    requestMessage.Headers.Add("Accept-Language", "de-DE");
+            HttpResponseMessage responseMessage = await _authenticatedServer
+                .CreateClient()
+                .AuthenticateWith(_staff)
+                .SendAsync(requestMessage);
 
-        //    HttpResponseMessage responseMessage = await _authenticatedServer
-        //        .CreateClient()
-        //        .AuthenticateWith(_staff)
-        //        .SendAsync(requestMessage);
+            IList<RoleDto> result = await DeserializeResponseMessageAsync<IList<RoleDto>>(responseMessage);
 
-        //    IList<RoleDto> result = await DeserializeResponseMessageAsync<IList<RoleDto>>(responseMessage);
+            result.AsQueryable().Where(s => s.Id == RoleDtoData.Performer.Id)
+                .Select(s => s.RoleName).First().Should().Be("Mitwirkender");
 
-        //    result.AsQueryable().Where(s => s.Id == RoleDtoData.Performer.Id)
-        //        .Select(s => s.RoleName).First().Should().Be("Künstler");
+            result.AsQueryable().Where(s => s.Id == RoleDtoData.Staff.Id).Select(s => s.RoleName)
+                .First().Should().Be("Mitarbeiter");
 
-        //    result.AsQueryable().Where(s => s.Id == RoleDtoData.Staff.Id).Select(s => s.RoleName)
-        //        .First().Should().Be("Mitarbeiter");
-
-        //    result.AsQueryable().Where(s => s.Id == RoleDtoData.Admin.Id).Select(s => s.RoleName)
-        //        .First().Should().Be("Administrator");
-        //}
+            result.AsQueryable().Where(s => s.Id == RoleDtoData.Admin.Id).Select(s => s.RoleName)
+                .First().Should().Be("Administrator");
+        }
     }
 }
