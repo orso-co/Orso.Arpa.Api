91e02b15c9b41ae7b943261853163d091fbabb55
Wolfgang Roese <mail@wolfgangroese.de>
2022-02-15 22:30:38 +0100

    put-endpoint for MyProjectParticipationStatus

--- Orso.Arpa.Api/Controllers/MyProjectsController.cs
+++ Orso.Arpa.Api/Controllers/MyProjectsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Orso.Arpa.Application.Interfaces;
+using Orso.Arpa.Application.MeApplication;
 using Orso.Arpa.Application.MyMusicianProfileApplication;
 using Orso.Arpa.Application.MyProjectApplication;
 using Orso.Arpa.Application.Services;
@@ -32,5 +33,22 @@ public class MyProjectsController : BaseController
     public async Task<ActionResult<IEnumerable<MyProjectDto>>> Get()
     {
         return Ok(await _myProjectService.GetMyProjectsAsync());
+    }
+
+    /// <summary>
+    /// Sets the participationstatus for a project for the current user
+    /// </summary>
+    /// <param name="setMyProjectParticipationStatus"></param>
+    /// <response code="200"></response>
+    /// <response code="404">If entity could not be found</response>
+    /// <response code="422">If validation fails</response>
+    [Authorize(Roles = RoleNames.Performer)]
+    [HttpPut("{id}/participation")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status422UnprocessableEntity)]
+    public async Task<ActionResult> SetProjectParticipationStatus(MyProjectParticipationModifyDto setMyProjectParticipationStatus)
+    {
+        return Ok( await _myProjectService.SetProjectParticipationStatus(setMyProjectParticipationStatus));
     }
 }
---
+++ Orso.Arpa.Domain/Logic/MyProjects/SetProjectParticipationStatus.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Orso.Arpa.Domain.Entities;
+using Orso.Arpa.Domain.Extensions;
+using Orso.Arpa.Domain.Interfaces;
+using Orso.Arpa.Domain.Logic.Me;
+using Orso.Arpa.Misc;
+using SetProjectParticipation = Orso.Arpa.Domain.Logic.Projects.SetProjectParticipation;
+
+namespace Orso.Arpa.Domain.Logic.MyProjects;
+
+public static class SetProjectParticipationStatus
+{
+    public class Command : IRequest<ProjectParticipation>
+    {
+        public string CommentByPerformerInner { get; set; }
+        public Guid ParticipationStatusInnerId { get; set; }
+        public Guid ProjectId { get; set; }
+        public Guid MusicianProfileId { get; set; }
+    }
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator(IArpaContext arpaContext, IDateTimeProvider dateTimeProvider, ITokenAccessor tokenAccessor)
+        {
+            RuleFor(c => c.ProjectId)
+                .Cascade(CascadeMode.Stop)
+                .EntityExists<Command, Project>(arpaContext)
+                .MustAsync(async (projectId, cancellation) =>
+                    !(await arpaContext.FindAsync<Project>(new object[] {projectId}, cancellation))
+                        .IsCompleted)
+                .WithMessage(
+                    "The project is completed. You may not set the participation of a completed project")
+                .MustAsync(async (command, projectId, cancellation) =>
+                    (await arpaContext.EntityExistsAsync<ProjectParticipation>(
+                        pp => pp.ProjectId == projectId &&
+                              pp.MusicianProfileId == command.MusicianProfileId, cancellation)))
+                .WithErrorCode("404")
+                .WithMessage("Project Participation could not be found.");
+
+            RuleFor(c => c.ParticipationStatusInnerId)
+                .SelectValueMapping<Command, ProjectParticipation>(arpaContext, p => p.ParticipationStatusInner);
+
+            RuleFor(c => c.MusicianProfileId)
+                .Cascade(CascadeMode.Stop)
+                .MustAsync(async (musicianProfileId, cancellation) =>
+                    !(await arpaContext.EntityExistsAsync<MusicianProfile>(
+                        d => d.Id == musicianProfileId && d.PersonId == tokenAccessor.PersonId ,
+                        cancellation)))
+                .WithErrorCode("404")
+                .WithMessage("Musician Profile could not be found.")
+                .MustAsync(async (musicianProfileId, cancellation) =>
+                    !(await arpaContext.EntityExistsAsync<MusicianProfileDeactivation>(
+                        d => d.MusicianProfileId == musicianProfileId && d.DeactivationStart <= dateTimeProvider.GetUtcNow(),
+                        cancellation)))
+                .WithMessage("The musician profile is deactivated. A deactivated musician profile may not participate in a project");
+
+        }
+    }
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Command, ProjectParticipation>()
+                .ForMember(dest => dest.ParticipationStatusInnerId, opt => opt.MapFrom(src => src.ParticipationStatusInnerId))
+                .ForMember(dest => dest.CommentByPerformerInner, opt => opt.MapFrom(src => src.CommentByPerformerInner))
+                .ForAllOtherMembers(opt => opt.Ignore());
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, ProjectParticipation>
+    {
+        private readonly IArpaContext _arpaContext;
+        private readonly IMapper _mapper;
+
+        public Handler(IArpaContext arpaContext, IMapper mapper)
+        {
+            _arpaContext = arpaContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ProjectParticipation> Handle(Command request, CancellationToken cancellationToken)
+        {
+            // Das MuPro muss einmal geladen werden, weil sonst die Property MusicianProfile des neuen Objekts null ist
+            ProjectParticipation participation = (await _arpaContext.FindAsync<MusicianProfile>(new object[] { request.MusicianProfileId }, cancellationToken))
+                .ProjectParticipations.FirstOrDefault(pp => pp.ProjectId == request.ProjectId);
+
+            _mapper.Map(request, participation);
+            participation = _arpaContext.ProjectParticipations.Update(participation).Entity;
+
+            if (await _arpaContext.SaveChangesAsync(cancellationToken) > 0)
+            {
+                return participation;
+            }
+
+            throw new Exception("Problem setting project participation");
+        }
+    }
+}
+
+
