7617cbc54a0d9f6f192b8f2034ca5308db3d7594
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2022-03-06 19:49:55 +0100

    ARPA-632 Fixed views (#406)

--- Orso.Arpa.Persistence/SqlStatements/ActiveUserAppointmentsForSectionView.sql
+++ Orso.Arpa.Persistence/SqlStatements/ActiveUserAppointmentsForSectionView.sql
@@ -1,31 +1,79 @@
-create or replace view active_user_appointments_for_sections as
--- Termine, bei denen Sections angegeben sind, zu denen der User mindestens ein Musikerprofil hat,
-select m.given_name, m.surname, m.person_id, m.mp_id, md.deactivation_start, a.id, a.name, a.public_details, a.internal_details, a.start_time, a.end_time
-  from appointments a, section_appointments sa, sections s, musician m left outer join musician_profile_deactivations md on m.mp_id=md.musician_profile_id
- where a.id = sa.appointment_id and sa.section_id = s.id and s.id = m.instrument_id 
-   and (a.start_time < md.deactivation_start or md.deactivation_start is null)
-   and a.deleted = false and sa.deleted = false and s.deleted = false
-union
--- Termine, bei denen Sections angegeben sind, zu denen der User mindestens ein Wechselinstrumente hat,
-select m.given_name, m.surname, m.person_id, m.mp_id, md.deactivation_start, a.id, a.name, a.public_details, a.internal_details, a.start_time, a.end_time
-  from appointments a,section_appointments sa, sections s, musician_profile_sections mps, musician m left outer join musician_profile_deactivations md on m.mp_id=md.musician_profile_id
- where a.id = sa.appointment_id and sa.section_id = s.id and s.id = mps.section_id and mps.musician_profile_id=m.mp_id
-   and a.deleted = false and sa.deleted = false and s.deleted = false and mps.deleted=false
-union
--- oder die Eltern-Knoten der Sections sind, zu denen der User Musikerprofile hat.
-select m.given_name, m.surname, m.person_id, m.mp_id, md.deactivation_start, a.id, a.name, a.public_details, a.internal_details, a.start_time, a.end_time
-  from appointments a, section_appointments sa, sections s, musician m left outer join musician_profile_deactivations md on m.mp_id=md.musician_profile_id
- where a.id = sa.appointment_id and sa.section_id = s.id 
-   and s.id in ( select sec_id from fn_list_parent_sections(m.instrument_id) )  
-   and (a.start_time < md.deactivation_start or md.deactivation_start is null)
-   and a.deleted = false and sa.deleted = false and s.deleted = false
-union 
--- oder die Eltern-Knoten der Sections sind, zu denen der User Wechselinstrumente hat.
-select m.given_name, m.surname, m.person_id, m.mp_id, md.deactivation_start, a.id, a.name, a.public_details, a.internal_details, a.start_time, a.end_time
-  from appointments a, section_appointments sa, sections s, musician_profile_sections mps, musician m left outer join musician_profile_deactivations md on m.mp_id=md.musician_profile_id
- where a.id = sa.appointment_id and sa.section_id = s.id 
-   and s.id = mps.section_id and mps.musician_profile_id=m.mp_id
-   and mps.section_id in ( select sec_id from fn_list_parent_sections(m.instrument_id) )
-   and (a.start_time < md.deactivation_start or md.deactivation_start is null)
-   and a.deleted = false and sa.deleted = false and s.deleted = false and mps.deleted=false
-;
+CREATE OR REPLACE VIEW public.active_user_appointments_for_sections
+ AS
+ SELECT m.given_name,
+    m.surname,
+    m.person_id,
+    m.mp_id,
+    md.deactivation_start,
+    a.id,
+    a.name,
+    a.public_details,
+    a.internal_details,
+    a.start_time,
+    a.end_time
+   FROM appointments a,
+    section_appointments sa,
+    sections s,
+    musician m
+     LEFT JOIN musician_profile_deactivations md ON m.mp_id = md.musician_profile_id
+  WHERE a.id = sa.appointment_id AND sa.section_id = s.id AND s.id = m.instrument_id AND (a.start_time < md.deactivation_start OR md.deleted = true OR md.deactivation_start IS NULL) AND a.deleted = false AND sa.deleted = false AND s.deleted = false
+UNION
+ SELECT m.given_name,
+    m.surname,
+    m.person_id,
+    m.mp_id,
+    md.deactivation_start,
+    a.id,
+    a.name,
+    a.public_details,
+    a.internal_details,
+    a.start_time,
+    a.end_time
+   FROM appointments a,
+    section_appointments sa,
+    sections s,
+    musician_profile_sections mps,
+    musician m
+     LEFT JOIN musician_profile_deactivations md ON m.mp_id = md.musician_profile_id
+  WHERE a.id = sa.appointment_id AND sa.section_id = s.id AND s.id = mps.section_id AND mps.musician_profile_id = m.mp_id AND a.deleted = false AND sa.deleted = false AND s.deleted = false AND mps.deleted = false
+UNION
+ SELECT m.given_name,
+    m.surname,
+    m.person_id,
+    m.mp_id,
+    md.deactivation_start,
+    a.id,
+    a.name,
+    a.public_details,
+    a.internal_details,
+    a.start_time,
+    a.end_time
+   FROM appointments a,
+    section_appointments sa,
+    sections s,
+    musician m
+     LEFT JOIN musician_profile_deactivations md ON m.mp_id = md.musician_profile_id
+  WHERE a.id = sa.appointment_id AND sa.section_id = s.id AND (s.id IN ( SELECT fn_list_parent_sections.sec_id
+           FROM fn_list_parent_sections(m.instrument_id) fn_list_parent_sections(sec_name, sec_id, sec_parent_id))) AND (a.start_time < md.deactivation_start OR md.deleted OR md.deactivation_start IS NULL) AND a.deleted = false AND sa.deleted = false AND s.deleted = false
+UNION
+ SELECT m.given_name,
+    m.surname,
+    m.person_id,
+    m.mp_id,
+    md.deactivation_start,
+    a.id,
+    a.name,
+    a.public_details,
+    a.internal_details,
+    a.start_time,
+    a.end_time
+   FROM appointments a,
+    section_appointments sa,
+    sections s,
+    musician_profile_sections mps,
+    musician m
+     LEFT JOIN musician_profile_deactivations md ON m.mp_id = md.musician_profile_id
+  WHERE a.id = sa.appointment_id AND sa.section_id = s.id AND s.id = mps.section_id AND mps.musician_profile_id = m.mp_id AND (mps.section_id IN ( SELECT fn_list_parent_sections.sec_id
+           FROM fn_list_parent_sections(m.instrument_id) fn_list_parent_sections(sec_name, sec_id, sec_parent_id))) AND (a.start_time < md.deactivation_start OR md.deleted OR md.deactivation_start IS NULL) AND a.deleted = false AND sa.deleted = false AND s.deleted = false AND mps.deleted = false;
+
+
