84d4d6e9228b467033f353b448f2d909f872578f
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2022-03-06 19:49:55 +0100

    ARPA-632 Fixed views (#406)

--- Orso.Arpa.Persistence/SqlStatements/ActiveUserAppointmentsForProjectView.sql
+++ Orso.Arpa.Persistence/SqlStatements/ActiveUserAppointmentsForProjectView.sql
@@ -1,8 +1,21 @@
-create or replace view active_user_appointments_for_projects as
-select m.given_name, m.surname, m.person_id, m.mp_id, md.deactivation_start, a.id, a.name, a.public_details, a.internal_details, a.start_time, a.end_time
-  from appointments a, project_appointments pa, projects p, project_participations pp, 
-       musician m left outer join musician_profile_deactivations md on m.mp_id=md.musician_profile_id
- where a.id=pa.appointment_id and pa.project_id=p.id and p.id=pp.project_id and pp.musician_profile_id=m.mp_id 
-   and (a.start_time < md.deactivation_start or md.deactivation_start is null)
-   and a.deleted=false and pa.deleted=false and p.deleted=false and pp.deleted=false 
-;
+CREATE OR REPLACE VIEW public.active_user_appointments_for_projects
+ AS
+ SELECT m.given_name,
+    m.surname,
+    m.person_id,
+    m.mp_id,
+    md.deactivation_start,
+    a.id,
+    a.name,
+    a.public_details,
+    a.internal_details,
+    a.start_time,
+    a.end_time
+   FROM appointments a,
+    project_appointments pa,
+    projects p,
+    project_participations pp,
+    musician m
+     LEFT JOIN musician_profile_deactivations md ON m.mp_id = md.musician_profile_id
+  WHERE a.id = pa.appointment_id AND pa.project_id = p.id AND p.id = pp.project_id AND pp.musician_profile_id = m.mp_id AND (a.start_time < md.deactivation_start OR md.deleted = true OR md.deactivation_start IS NULL) AND a.deleted = false AND pa.deleted = false AND p.deleted = false AND pp.deleted = false;
+
