daf60ed09a7112950d8a237ec503f3c801adf101
wolfgangroese <mail@wolfgangroese.de>
2022-07-19 22:23:09 +0200

    added migrations

---
+++ Orso.Arpa.Persistence/Migrations/20220719202108_AddCommentByPerformerInAppointments.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    public partial class AddCommentByPerformerInAppointments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "comment_by_performer_inner",
+                table: "appointment_participations",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "comment_by_performer_inner",
+                table: "appointment_participations");
+        }
+    }
+}
