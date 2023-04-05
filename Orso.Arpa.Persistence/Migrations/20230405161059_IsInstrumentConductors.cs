823b585e3956bdb8e9a92db628c67ec88ec2fb56
wolfgangroese <wolfgang.roese@orso.co>
2023-04-05 18:13:06 +0200

    set IsInstrument=true in musical staff-sections like conductor etc.

---
+++ Orso.Arpa.Persistence/Migrations/20230405161059_IsInstrumentConductors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IsInstrumentConductors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "sections",
+                keyColumn: "id",
+                keyValue: new Guid("4e7a61c5-d2e4-4e3b-b21d-34a90cf958b2"),
+                column: "is_instrument",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "sections",
+                keyColumn: "id",
+                keyValue: new Guid("4e7a61c5-d2e4-4e3b-b21d-34a90cf958b2"),
+                column: "is_instrument",
+                value: false);
+        }
+    }
+}
