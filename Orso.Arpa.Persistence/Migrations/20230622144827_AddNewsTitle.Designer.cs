d703b2a944d1e20bedce29dd7906bc42ff8bcae1
wolfgangroese <wolfgang.roese@orso.co>
2023-06-22 16:50:03 +0200

    added migration

--- Orso.Arpa.Persistence/Migrations/20230622144827_AddNewsTitle.Designer.cs
+++ Orso.Arpa.Persistence/Migrations/20230622144827_AddNewsTitle.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Orso.Arpa.Persistence.DataAccess;
@@ -11,9 +12,11 @@ using Orso.Arpa.Persistence.DataAccess;
 namespace Orso.Arpa.Persistence.Migrations
 {
     [DbContext(typeof(ArpaContext))]
-    partial class ArpaContextModelSnapshot : ModelSnapshot
+    [Migration("20230622144827_AddNewsTitle")]
+    partial class AddNewsTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Orso.Arpa.Persistence/Migrations/20230622144827_AddNewsTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orso.Arpa.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewsTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "news_title",
+                table: "news",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "news_title",
+                table: "news");
+        }
+    }
+}
