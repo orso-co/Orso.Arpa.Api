bec85b61d67c301080c38a6dc8dd5e78872ece52
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2023-07-02 19:37:13 +0200

    code adaptions to support linux and macos (#751)

--- Orso.Arpa.Misc/ArpaQRCodeGenerator.cs
+++ Orso.Arpa.Misc/ArpaQRCodeGenerator.cs
@@ -1,25 +1,26 @@
-using System.Drawing;
-using System.Drawing.Imaging;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using QRCoder;
+using SixLabors.ImageSharp;
 
 namespace Orso.Arpa.Misc
 {
     public static class ArpaQRCodeGenerator
     {
-        public static byte[] GetQRCode(string textToEncode)
+        public static async Task<byte[]> GetQRCodeAsync(string textToEncode, CancellationToken cancellationToken)
         {
             var qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(textToEncode, QRCodeGenerator.ECCLevel.Q);
-            var qrCode = new QRCoder.QRCode(qrCodeData);
-            Bitmap qrCodeImage = qrCode.GetGraphic(20);
-            return BitmapToBytes(qrCodeImage);
+            var qrCode = new QRCode(qrCodeData);
+            using Image qrCodeImage = qrCode.GetGraphic(20);
+            return await ImageToBytesAsync(qrCodeImage, cancellationToken);
         }
 
-        private static byte[] BitmapToBytes(Bitmap img)
+        private static async Task<byte[]> ImageToBytesAsync(Image image, CancellationToken cancellationToken)
         {
             using var stream = new MemoryStream();
-            img.Save(stream, ImageFormat.Png);
+            await image.SaveAsPngAsync(stream, cancellationToken);
             return stream.ToArray();
         }
     }
