d388dce8d3b59003869ddc3c1c44f3edf15cbe5a
VILLAN3LL3 <development@gutfrau.de>
2023-07-16 13:47:35 +0200

    ARPA-932 Send slack notification on email confirmed

--- Orso.Arpa.Domain/Logic/Auth/EmailConfirmedNotification.cs
+++ Orso.Arpa.Domain/Logic/Auth/EmailConfirmedNotification.cs
@@ -7,45 +7,47 @@ using Orso.Arpa.Domain.Configuration;
 using Orso.Arpa.Domain.Entities;
 using Orso.Arpa.Domain.Identity;
 using Orso.Arpa.Misc;
+using Orso.Arpa.Misc.Extensions;
 using Orso.Arpa.Misc.Logging;
 
 namespace Orso.Arpa.Domain.Logic.Auth
 {
-    public class UserRegisteredNotification : INotification
+    public class EmailConfirmedNotification : INotification
     {
         public string UserName { get; set; }
     }
 
-    public class SendUserRegisteredInfoToKbb : INotificationHandler<UserRegisteredNotification>
+    public class SendUEmailConfirmedInfoToKbb : INotificationHandler<EmailConfirmedNotification>
     {
         private readonly ArpaUserManager _userManager;
         private readonly JwtConfiguration _jwtConfiguration;
-        private readonly ILogger<SendUserRegisteredInfoToKbb> _logger;
+        private readonly ILogger<SendUEmailConfirmedInfoToKbb> _logger;
 
-        public SendUserRegisteredInfoToKbb(
+        public SendUEmailConfirmedInfoToKbb(
             ArpaUserManager userManager,
             JwtConfiguration jwtConfiguration,
-            ILogger<SendUserRegisteredInfoToKbb> logger)
+            ILogger<SendUEmailConfirmedInfoToKbb> logger)
         {
             _userManager = userManager;
             _jwtConfiguration = jwtConfiguration;
             _logger = logger;
         }
 
-        public async Task Handle(UserRegisteredNotification notification, CancellationToken cancellationToken)
+        public async Task Handle(EmailConfirmedNotification notification, CancellationToken cancellationToken)
         {
             User user = await _userManager.FindByNameAsync(notification.UserName);
 
             KbbInfoLogger.LogInfoForKbb(
                 _logger,
-                "user registered",
+                "email confirmed",
                 new Dictionary<string, object>
                 {
                     { "Person", user.Person },
                     { "E-Mail", user.Email },
+                    { "Registration Date", user.CreatedAt.ToGermanDateTimeString() },
                     { "Link", $"{_jwtConfiguration.Audience}/arpa/dashboard/admin".FormatLink("Go to Admin Dashboard") }
                 },
-                "The user must now confirm their email address");
+                "The user is now waiting for their role assignment");
         }
     }
 }
