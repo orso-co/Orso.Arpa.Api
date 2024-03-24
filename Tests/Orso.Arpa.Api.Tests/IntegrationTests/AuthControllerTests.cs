33af4f484e81c282a0d97acc48527dc42cb02e85
handesirikci0rso <hande.sirikci@orso.co>
2024-03-24 14:44:20 +0100

    try

--- Tests/Orso.Arpa.Api.Tests/IntegrationTests/AuthControllerTests.cs
+++ Tests/Orso.Arpa.Api.Tests/IntegrationTests/AuthControllerTests.cs
@@ -20,7 +20,7 @@ namespace Orso.Arpa.Api.Tests.IntegrationTests
 {
     public class AuthControllerTests : IntegrationTestBase
     {
-        [Test, Order(1)]
+        [Test, Order(15)]
         public async Task Should_Login()
         {
             // Arrange
@@ -218,7 +218,7 @@ namespace Orso.Arpa.Api.Tests.IntegrationTests
             resetResponseMessage.StatusCode.Should().Be(HttpStatusCode.NoContent);
         }
 
-        [Test, Order(15)]
+        [Test, Order(1)]
         public async Task Should_Not_Register_Existing_Email()
         {
             // Arrange
