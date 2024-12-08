04f76da4d52c8d8d01b970a4507ca830de532bf7
VILLAN3LL3 <development@gutfrau.de>
2024-12-08 16:29:29 +0100

    added user mail for staff to ReducedPersonDto

--- Orso.Arpa.Application/PersonApplication/Model/ReducedPersonDto.cs
+++ Orso.Arpa.Application/PersonApplication/Model/ReducedPersonDto.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Text.Json.Serialization;
 using AutoMapper;
+using Orso.Arpa.Application.General.MappingActions;
 using Orso.Arpa.Domain.PersonDomain.Model;
+using Orso.Arpa.Domain.UserDomain.Enums;
 
 namespace Orso.Arpa.Application.PersonApplication.Model
 {
@@ -10,13 +13,19 @@ namespace Orso.Arpa.Application.PersonApplication.Model
         public string GivenName { get; set; }
         public string Surname { get; set; }
         public string DisplayName { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [IncludeForRoles(RoleNames.Staff)]
+        public string UserEmail { get; set; }
     }
 
     public class ReducedPersonDtoMappingProfile : Profile
     {
         public ReducedPersonDtoMappingProfile()
         {
-            CreateMap<Person, ReducedPersonDto>();
+            CreateMap<Person, ReducedPersonDto>()
+              .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User != null ? src.User.Email : string.Empty))
+              .AfterMap<RoleBasedSetNullAction<Person, ReducedPersonDto>>();
         }
     }
 }
