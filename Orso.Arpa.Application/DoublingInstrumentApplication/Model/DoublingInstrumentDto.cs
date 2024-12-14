635b0a80a7c1bca41c15aba64eba5df1d98bcfb4
VILLAN3LL3 <development@gutfrau.de>
2024-12-14 13:09:09 +0100

    exteded doubling instrument dto

--- Orso.Arpa.Application/DoublingInstrumentApplication/Model/DoublingInstrumentDto.cs
+++ Orso.Arpa.Application/DoublingInstrumentApplication/Model/DoublingInstrumentDto.cs
@@ -1,6 +1,8 @@
 using System;
 using AutoMapper;
 using Orso.Arpa.Application.General.Model;
+using Orso.Arpa.Application.SectionApplication.Model;
+using Orso.Arpa.Application.SelectValueApplication.Model;
 using Orso.Arpa.Domain.General.Model;
 using Orso.Arpa.Domain.MusicianProfileDomain.Model;
 
@@ -13,6 +15,8 @@ namespace Orso.Arpa.Application.DoublingInstrumentApplication.Model
         public byte LevelAssessmentTeam { get; set; }
         public Guid? AvailabilityId { get; set; }
         public string Comment { get; set; }
+        public SectionDto Instrument { get; set; }
+        public SelectValueDto Availability { get; set; }
     }
 
     public class DoublingInstrumentDtoMappingProfile : Profile
@@ -25,6 +29,8 @@ namespace Orso.Arpa.Application.DoublingInstrumentApplication.Model
                 .ForMember(dest => dest.LevelAssessmentInner, opt => opt.MapFrom(src => src.LevelAssessmentInner))
                 .ForMember(dest => dest.LevelAssessmentTeam, opt => opt.MapFrom(src => src.LevelAssessmentTeam))
                 .ForMember(dest => dest.Comment, opt => opt.MapFrom(src => src.Comment))
+                .ForMember(dest => dest.Instrument, opt => opt.MapFrom(src => src.Section))
+                .ForMember(dest => dest.Availability, opt => opt.MapFrom(src => src.InstrumentAvailability))
                 .IncludeBase<BaseEntity, BaseEntityDto>();
         }
     }
