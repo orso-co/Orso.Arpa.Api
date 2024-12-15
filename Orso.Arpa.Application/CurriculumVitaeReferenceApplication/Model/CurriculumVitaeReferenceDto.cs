2575af452dfa92ae123897fc94fbe390e822ed72
VILLAN3LL3 <development@gutfrau.de>
2024-12-15 18:36:15 +0100

    added type to curriculumvitaereferencedto

--- Orso.Arpa.Application/CurriculumVitaeReferenceApplication/Model/CurriculumVitaeReferenceDto.cs
+++ Orso.Arpa.Application/CurriculumVitaeReferenceApplication/Model/CurriculumVitaeReferenceDto.cs
@@ -1,6 +1,7 @@
 using System;
 using AutoMapper;
 using Orso.Arpa.Application.General.Model;
+using Orso.Arpa.Application.SelectValueApplication.Model;
 using Orso.Arpa.Domain.General.Model;
 using Orso.Arpa.Domain.MusicianProfileDomain.Model;
 
@@ -13,6 +14,7 @@ namespace Orso.Arpa.Application.CurriculumVitaeReferenceApplication.Model
         public Guid? TypeId { get; set; }
         public string Description { get; set; }
         public byte? SortOrder { get; set; }
+        public SelectValueDto Type { get; set; }
     }
 
     public class CurriculumVitaeReferenceDtoMappingProfile : Profile
