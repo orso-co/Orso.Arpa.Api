ab57ed42c5002766ba8e087709e980b31dadd1e3
VILLAN3LL3 <development@gutfrau.de>
2024-12-30 15:14:07 +0100

    unit test fix

--- Tests/Orso.Arpa.Misc.Tests/Extensions/StringExtensionsTests.cs
+++ Tests/Orso.Arpa.Misc.Tests/Extensions/StringExtensionsTests.cs
@@ -1,4 +1,3 @@
-using System;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -17,6 +16,73 @@ namespace Orso.Arpa.Misc.Tests.Extensions
             var result = "FaDiNa_Auswahl_2020-02-25_85836_©_A.Linsenmann.jpg".RemoveEverythingButAscii();
 
             result.Should().Be("FaDiNa_Auswahl_2020-02-25_85836__A.Linsenmann.jpg");
+        }
+
+        [Test]
+        public void CreateGuid_ShouldReturnEmptyGuid_WhenStringIsNull()
+        {
+            // Arrange
+            string? input = null;
+
+            // Act
+            Guid result = input.CreateGuid();
+
+            // Assert
+            result.Should().Be(Guid.Empty);
+        }
+
+        [Test]
+        public void CreateGuid_ShouldReturnEmptyGuid_WhenStringIsEmpty()
+        {
+            // Arrange
+            string input = string.Empty;
+
+            // Act
+            Guid result = input.CreateGuid();
+
+            // Assert
+            result.Should().Be(Guid.Empty);
+        }
+
+        [Test]
+        public void CreateGuid_ShouldReturnEmptyGuid_WhenStringIsWhitespace()
+        {
+            // Arrange
+            string input = "   ";
+
+            // Act
+            Guid result = input.CreateGuid();
+
+            // Assert
+            result.Should().Be(Guid.Empty);
+        }
+
+        [Test]
+        public void CreateGuid_ShouldReturnDeterministicGuid_WhenStringIsValid()
+        {
+            // Arrange
+            string input = "Hello world!";
+            Guid expectedGuid = Guid.Parse("9d26fb86-0d19-852c-f6e0-468ceca42a20");
+
+            // Act
+            Guid result = input.CreateGuid();
+
+            // Assert
+            result.Should().Be(expectedGuid);
+        }
+
+        [Test]
+        public void CreateGuid_ShouldReturnSameGuid_ForSameString()
+        {
+            // Arrange
+            string input = "sameString";
+
+            // Act
+            Guid result1 = input.CreateGuid();
+            Guid result2 = input.CreateGuid();
+
+            // Assert
+            result1.Should().Be(result2);
         }
     }
 }
