920b192caec5b1d4f97ea3f94ce60691ce4147ab
VILLAN3LL3 <development@gutfrau.de>
2024-12-31 11:10:28 +0100

    removed venuelistdto

--- Tests/Orso.Arpa.Tests.Shared/DtoTestData/VenueDtoData.cs
+++ Tests/Orso.Arpa.Tests.Shared/DtoTestData/VenueDtoData.cs
@@ -8,30 +8,13 @@ namespace Orso.Arpa.Tests.Shared.DtoTestData
 {
     public static class VenueDtoData
     {
-        public static IList<VenueListDto> Venues
+        public static IList<VenueDto> Venues
         {
             get
             {
                 return
                 [
-                    new VenueListDto {
-                        Id = Guid.Parse("54eb30ff-6ea3-4026-8a49-5f149c8ec7e1"),
-                        Name = "Weiherhof Schule",
-                        Description = "Proberäume",
-                        CreatedBy = "anonymous",
-                        CreatedAt = FakeDateTime.UtcNow,
-                        Address = new AddressDto {
-                            Id = Guid.Parse("9dfd22c2-41c6-463c-a4cd-334215584d56"),
-                            Address1 = "Schlüsselstraße 5",
-                            Zip = "79104",
-                            City = "Freiburg",
-                            Country = "Deutschland",
-                            UrbanDistrict = "Herdern",
-                            State = "Baden-Württemberg",
-                            CreatedBy = "anonymous",
-                            CreatedAt = FakeDateTime.UtcNow
-                        }
-                    }
+                    WeiherhofSchule
                 ];
             }
         }
