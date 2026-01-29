f1265da57d4e96e8c2c30e94bee1081c248b24cc
Wolfgang Roese <mail@wolfgangroese.de>
2026-01-29 01:35:14 +0100

    fix: Manually map MusicianProfile with Person for stage positions

--- Orso.Arpa.Application/StageSetupApplication/Model/StageSetupPositionDto.cs
+++ Orso.Arpa.Application/StageSetupApplication/Model/StageSetupPositionDto.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using AutoMapper;
 using Orso.Arpa.Application.General.Model;
 using Orso.Arpa.Application.MusicianProfileApplication.Model;
+using Orso.Arpa.Application.PersonApplication.Model;
 using Orso.Arpa.Domain.General.Model;
 using Orso.Arpa.Domain.StageSetupDomain.Model;
 
@@ -28,7 +30,28 @@ namespace Orso.Arpa.Application.StageSetupApplication.Model
         {
             CreateMap<StageSetupPosition, StageSetupPositionDto>()
                 .IncludeBase<BaseEntity, BaseEntityDto>()
-                .ForMember(dest => dest.MusicianProfile, opt => opt.MapFrom(src => src.MusicianProfile));
+                .ForMember(dest => dest.MusicianProfile, opt => opt.MapFrom((src, dest, destMember, context) =>
+                {
+                    if (src.MusicianProfile == null) return null;
+
+                    var mp = src.MusicianProfile;
+                    return new ReducedMusicianProfileDto
+                    {
+                        Id = mp.Id,
+                        InstrumentName = mp.Instrument?.Name,
+                        Qualification = mp.Qualification?.SelectValue?.Name ?? string.Empty,
+                        DoublingInstrumentNames = mp.DoublingInstruments?.Select(di => di.Section?.Name).Where(n => n != null).ToList() ?? [],
+                        PreferredPositionsTeam = mp.PreferredPositionsTeam?.Select(p => p.SelectValueSection?.SelectValue?.Name).Where(n => n != null).ToList() ?? [],
+                        Deactivation = mp.Deactivation != null ? context.Mapper.Map<MusicianProfileDeactivationApplication.Model.MusicianProfileDeactivationDto>(mp.Deactivation) : null,
+                        Person = mp.Person != null ? new PersonDto
+                        {
+                            Id = mp.Person.Id,
+                            DisplayName = mp.Person.DisplayName,
+                            GivenName = mp.Person.GivenName,
+                            Surname = mp.Person.Surname
+                        } : null
+                    };
+                }));
         }
     }
 }
